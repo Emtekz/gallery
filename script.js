1a2bb05c20f145ba11cf18a49f91d1117f38fcca
Paul Reiter <reiterpaul02@gmail.com>
2023-05-16 10:15:57 +0200

    Push

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ document.querySelectorAll('.image-container img').forEach(image => {
 document.querySelector('.popup-image span').onclick = () => {
     document.querySelector('.popup-image').style.display = 'none';
 }
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    if (evt.keyCode == 27) {
+        document.querySelector('.popup-image').style.display = 'none';  
+    }
+};
